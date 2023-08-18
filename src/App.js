9bdbcc7262e63ef1c6dc65044f4288424b43f4fa
chinkiat <e0544173@u.nus.edu>
2023-08-18 22:10:33 +0800

    mobile view

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import "./App.css";
+import { useState, useEffect } from "react";
 import JobCard from "./components/job-card/jobCard";
 import JobTitle from "./components/job-title/jobTitle";
 import jobData from "./data/job_data.json";
-import { useState } from "react";
+import "./App.css";
+
+const MOBILE_VIEWPORT_WIDTH = 720;
 
 function App() {
   const [selected, setSelected] = useState(0);
+  const [showRightPanel, setShowRightPanel] = useState(true);
+
+  function handleWindowSizeChange() {
+    setShowRightPanel(window.innerWidth > MOBILE_VIEWPORT_WIDTH);
+  }
+  useEffect(() => {
+    window.addEventListener("resize", handleWindowSizeChange);
+    return () => {
+      window.removeEventListener("resize", handleWindowSizeChange);
+    };
+  }, []);
 
   const renderJobCards = () => {
     return jobData.map((job, index) => {
@@ -19,13 +32,20 @@ function App() {
       );
     });
   };
-  console.log(window.innerWidth);
+
+  const renderRightPanel = () => {
+    return (
+      showRightPanel && (
+        <div className="right-column">
+          <JobTitle title={jobData[selected].title} />
+        </div>
+      )
+    );
+  };
   return (
     <div className="root">
       <div className="left-column">{renderJobCards()}</div>
-      <div className="right-column">
-        <JobTitle title={jobData[selected].title} />
-      </div>
+      {renderRightPanel()}
     </div>
   );
 }
